c19aa880efca950e2fc7b3df7a0300245c3f46fd
Roman Levchenkov <rlevch@gmail.com>
2018-09-09 20:45:15 +0300

    JavaScripts

---
+++ DutchTreat/wwwroot/js/index.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function () {
+
+    var x = 0;
+    var s = "";
+
+    console.log("Hello Software Developer");
+
+    var TheForm = $("#theForm");
+    TheForm.hide();
+
+    var button = $("#buyButton");
+    button.on("click", function () {
+        console.log("Buying Item");
+    });
+
+    var productInfo = $(".product-props li");
+    productInfo.on("click", function () {
+        console.log("You clicked on " + $(this).text());
+    });
+
+});

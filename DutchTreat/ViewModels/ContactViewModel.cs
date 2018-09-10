07f86e0974e0f1d4dc140e7bb2852386c3bdf2b2
Roman Levchenkov <rlevch@gmail.com>
2018-09-10 18:01:21 +0300

    MVC Complete

---
+++ DutchTreat/ViewModels/ContactViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DutchTreat.ViewModels
+{
+    public class ContactViewModel
+    {
+        [Required]
+        [MinLength(5)]
+        public string Name { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Subject { get; set; }
+        [Required]
+        [MaxLength(250, ErrorMessage = "The Message is too long and must be with a maximum length of '250'.")]
+        public string Message { get; set; }
+    }
+}
